f6f4f923f78d92383700a05989caac045768a7a3
qianxuege <qianxuege15@gmail.com>
2025-10-07 19:56:35 +0000

    fix: updated src/categories/index.js to follow the same behavior as the controller

--- src/categories/index.js
+++ src/categories/index.js
@@ -46,6 +46,12 @@ Categories.existsByHandle = async function (handle) {
 };
 
 Categories.getCategoryById = async function (data) {
+	// --- Normalize legacy sort=endorsed behavior ---
+	if (data.query && data.query.sort === 'endorsed') {
+		data.query.filter = 'endorsed';
+		delete data.query.sort;
+	}
+	
 	const categories = await Categories.getCategories([data.cid]);
 	if (!categories[0]) {
 		return null;
