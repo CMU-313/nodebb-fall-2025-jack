687cb281b1b81efbc640e1a4b2c15c46e9ea57a3
joon kwon <jkwon0429@gmail.com>
2025-10-27 21:39:42 +0000

    fixing some bugs that caused lint to fail

--- test/k6/k6-load-test-2.js
+++ test/k6/k6-load-test-2.js
@@ -3,24 +3,24 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
-  stages: [
-    { duration: '10s', target: 5 },  // ramp up
-    { duration: '20s', target: 5 },  // stay steady
-    { duration: '10s', target: 0 },  // ramp down
-  ],
+	stages: [
+		{ duration: '10s', target: 5 }, // ramp up
+		{ duration: '20s', target: 5 }, // stay steady
+		{ duration: '10s', target: 0 }, // ramp down
+	],
 };
 
 export default function () {
-  // simulate different user interactions
-  const urls = [
-    'http://128.2.221.71:4567',
-    'http://128.2.221.71:4567/categories',
-  ];
-  const randomUrl = urls[Math.floor(Math.random() * urls.length)];
+	// simulate different user interactions
+	const urls = [
+		'http://128.2.221.71:4567',
+		'http://128.2.221.71:4567/categories',
+	];
+	const randomUrl = urls[Math.floor(Math.random() * urls.length)];
 
-  const res = http.get(randomUrl, {
-    headers: { 'User-Agent': 'k6-team26' },
-  });
+	http.get(randomUrl, {
+		headers: { 'User-Agent': 'k6-team26' },
+	});
 
-  sleep(1);
+	sleep(1);
 }
